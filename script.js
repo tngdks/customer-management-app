8e02e89f41259497dce8da58a5615bd260e53b2b
tngdks <mrdsharmar363@gmail.com>
2026-04-13 11:48:37 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,73 @@
+let customers = JSON.parse(localStorage.getItem("customers")) || [];
+displayCustomers();
+
+function addCustomer (){
+    let input = document.getElementById("nameInput");
+    let name = input.value;
+
+    if(name === ""){
+        alert("Enter a name");
+        return;
+    }
+
+    customers.push(name);
+
+    localStorage.setItem("customers", JSON.stringify(customers));
+
+    displayCustomers();
+
+    input.value = "";
+}
+
+function displayCustomers(){
+    let list = document.getElementById("customerList");
+    list.innerHTML = "";
+
+    if(customers.length === 0) {
+        list.innerHTML = "No customers addded";
+        return;
+    }
+
+    for (let i = 0; i < customers.length; i++){
+        let li = document.createElement("li");
+        li.textContent = customers[i];
+
+        //create delete button
+        let delBtn = document.createElement("button");
+        delBtn.textContent = "Delete";
+        
+        //when clicked, delete this item
+        delBtn.onclick = function(){
+            deleteCustomer(i);
+        };
+
+        //create edit button
+        let editBtn = document.createElement("button");
+        editBtn.textContent = "Edit";
+        editBtn.onclick = function(){
+            editCustomer(i);
+        }
+
+        li.appendChild(editBtn)
+        li.appendChild(delBtn);
+        list.appendChild(li);
+    }
+}
+
+function deleteCustomer(index){
+    customers.splice(index, 1);
+    localStorage.setItem("customers", JSON.stringify(customers));
+    displayCustomers();
+}
+
+function editCustomer(index){
+    let newName = prompt("Enter new name:");
+
+    if (newName !== null && newName !== ""){
+        customers[index] = newName;
+
+        localStorage.setItem("customers", JSON.stringify(customers));
+
+        displayCustomers();
+    }
+}
